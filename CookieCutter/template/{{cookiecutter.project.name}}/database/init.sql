6298f062d8c67fba6c6c523b399f2eeed086e23d
hugozalm <hugozalm@gmail.com>
2025-12-02 16:15:32 +0100

    removed cookiecutter

--- CookieCutter/template/{{cookiecutter.project.name}}/database/init.sql
+++
@@ -1,7 +0,0 @@
--- Initialize database
-CREATE EXTENSION IF NOT EXISTS postgis;
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
--- Create keycloak database if it doesn't exist
-SELECT 'CREATE DATABASE keycloak'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'keycloak')\gexec
